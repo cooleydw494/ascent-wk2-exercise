e02899d4b35e6369ea0e69860bf4597c0523ea0b
cooleydw494 <cooleydw494@gmail.com>
2022-04-05 03:16:01 -0500

    implement showBalance

--- index.mjs
+++ index.mjs
@@ -8,14 +8,10 @@ const stdlib = loadStdlib(process.env);
 console.log(`The consensus network is ${stdlib.connector}.`);
 
 const suStr = stdlib.standardUnit;
-const auStr = stdlib.atomicUnit;
 const toAU = (su) => stdlib.parseCurrency(su);
 const toSU = (au) => stdlib.formatCurrency(au, 4);
-const suBal = 1000;
-console.log(`Balance is ${suBal} ${suStr}`);
-const auBal = toAU(suBal);
-console.log(`Balance is ${auBal} ${auStr}`);
-console.log(`Balance is ${toSU(auBal)} ${suStr}`);
+const iBalance = toAU(1000);
+const showBalance = async (acc) => console.log(`Your balance is ${toSU(await stdlib.balanceOf(acc))} ${suStr}.`);
 
 (async () => {
   const commonInteract = {};
@@ -24,9 +20,11 @@ console.log(`Balance is ${toSU(auBal)} ${suStr}`);
   if (role === 'seller') {
     const sellerInteract = { ...commonInteract };
 
-    const acc = await stdlib.newTestAccount(stdlib.parseCurrency(1000));
+    const acc = await stdlib.newTestAccount(iBalance);
+    await showBalance(acc);
     const ctc = acc.contract(backend);
     await backend.Seller(ctc, sellerInteract);
+    await showBalance(acc);
   }
 
   // BUYER
